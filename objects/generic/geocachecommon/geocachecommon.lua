37b61d4ed92fa25b04d44b684597084e301024c8
zaphodthegrate <theassociate@gmail.com>
2014-01-21 15:53:23 -0600

    0.2.4 popup test
    knows owner and saves new visitors
    needs trade interface
    needs world name

--- objects/generic/geocachecommon/geocachecommon.lua
+++ objects/generic/geocachecommon/geocachecommon.lua
@@ -1,32 +1,80 @@
-function init(args)
-end
 
-function main()
-	if self.initialized == nil then
-    	initializeObject();
-		self.initialized = true;
+
+function init(args)
+	entity.setInteractive(true);
+	
+	if not storage.geocache then
+		--[[local loadedData = entity.configParameter("geocache");
+		if loadedData then
+			storage.geocache = loadedData;
+			entity.setAnimationState("geocacheState", "inactive");
+		else
+			entity.setAnimationState("geocacheState", "inactive");
+		end--]]
+		entity.setAnimationState("geocacheState", "inactive");
+	else
+		entity.setAnimationState("geocacheState", "inactive");
 	end
 end
 
-function initializeObject()
-	entity.setInteractive(true);
-	entity.setAnimationState("geocachestate", "inactive");
+function main()
 	
-	if self.tradingConfig == nil then
-		self.tradingConfig = buildTradingConfig();
+	if storage.geocache then
+		--ping();
 	end
 	
 end
 
 function onInteraction(args)
-	--return { "OpenCraftingInterface", self.tradingConfig }
-	entity.setAnimationState)"geocachestate", "active");
-end
+	local pid = args["sourceId"]
+	if not storage.geocache then
+		
+		--first placement
+		storage.geocache = {
+			--currentPlanet = world.info(), for when they add this back (?)
+			currentPosition = entity.position(),
+			ownerId = world.entityUuid(pid),
+			ownerName = world.entityName(pid),
+			playerFTF = nil,
+			finders = {},
+			item = {}
+		}
+		
+		entity.setAnimationState("geocacheState", "active");
+		
+		return { "ShowPopup", { message = "^white;Planet Name: ^red;" .. "not yet implemented" ..
+								"\n^white;Owner: ^green;" .. storage.geocache.ownerName
+		}}
+		
 
-function buildTradingConfig ()
-
-local currentPlanet = world.info();
-local currentPosition = entity.position();
-
-
+		
+	else
+		
+		local currentPlayer = world.entityName(pid);
+		
+		-- first to find
+		--if not storage.geocache.playerFTF and storage.geocache.ownerId ~= world.entityUuid(pid) then
+		if not storage.geocache.playerFTF then	
+			storage.geocache.playerFTF = currentPlayer;
+			table.insert(storage.geocache.finders, currentPlayer);
+		end
+	
+	    table.insert(storage.geocache.finders, currentPlayer);
+	
+		entity.setAnimationState("geocacheState", "active");
+	
+		local finderList = "\n^white;"
+		
+		for _, finder in pairs(storage.geocache.finders) do
+			finderList = finderList .. "\n" .. finder
+		end
+	
+		return { "ShowPopup", { message = "^white;Planet Name: ^red;" .. "not yet implemented" ..
+								"\n^white;Owner: ^green;" .. storage.geocache.ownerName ..
+								"\n^white;FTF: ^blue;" .. storage.geocache.playerFTF ..
+								"\n^white;Finder log: ^white;" .. finderList
+		}}
+	
+	end
+	
 end
